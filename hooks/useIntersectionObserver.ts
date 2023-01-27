1bcfb0d6852e927fb559dc72b94472b346fa719d
haryan248 <haryan96@naver.com>
2023-01-27 15:18:24 +0900

    feat: dropdown 컴포넌트 개발

---
+++ hooks/useIntersectionObserver.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState, useRef, RefObject } from 'react';
+
+const useOnScreen = <T extends HTMLElement>(
+  callback: (entry: IntersectionObserverEntry) => void,
+  option: IntersectionObserverInit | undefined = { threshold: 1 },
+) => {
+  const observer = useRef<IntersectionObserver>();
+  useEffect(() => {
+    observer.current = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        (() => callback(entry))();
+        // if (entry.isIntersecting) {
+        //   callback();
+        // }
+      });
+    }, option);
+  }, [callback, option]);
+
+  const observe = (element: T) => {
+    observer.current?.observe(element);
+  };
+
+  const unobserve = (element: T) => {
+    observer.current?.unobserve(element);
+  };
+
+  return [observe, unobserve];
+};
+
+export default useOnScreen;
