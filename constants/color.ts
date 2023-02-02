445750af89a1efda05e9d3714b566f6bbfaae082
zerocy18 <zerocy18@gmail.com>
2023-02-02 23:13:36 +0900

    feat : color constant 처리

--- constants/color.ts
+++ constants/color.ts
@@ -1,3 +1,19 @@
+export const BACKGROUND_COLOR = {
+  NAVY_1: '#191F28',
+  NAVY_2: '#1B222C',
+  NAVY_3: '#1E252F',
+  NAVY_4: '#252E38',
+  FIELD_10: 'rgba(255, 255, 255, 0.1)',
+};
+
+export const FONT_COLOR = {
+  GRAY_1: '#343B43',
+  GRAY_2: '#636C78',
+  GRAY_3: '#C5CAD0',
+  GRAY_4: '#E0E6EE',
+  WHITE: '#FFFFFF',
+};
+
 export const POINT_COLOR = {
   MAIN: '#22FFA2',
   SAVE: '#8BFFFF',
