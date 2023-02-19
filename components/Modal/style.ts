d08b8446ad09eb1a8bc6e0c46725fc17856ecc8e
zerocy18 <zerocy18@gmail.com>
2023-02-20 00:02:52 +0900

    feat : Modal Component 반응형 디자인 추가

--- components/Modal/style.ts
+++ components/Modal/style.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { FONT_COLOR, BACKGROUND_COLOR } from '@/constants/color';
 import Close from '@/assets/svgs/ic_x.svg';
+import { mq } from '@/styles/mediaQuery';
 
 const OutLayer = styled.div`
   z-index: 2;
@@ -17,20 +18,24 @@ const OutLayer = styled.div`
 const ModalContainer = styled.div`
   /* position: relative; */
   position: absolute;
-  width: 656px;
-  height: 521px;
+  width: 100%;
+  height: 100%;
   // Modal Component 항상 중앙유지
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
 
   background: ${BACKGROUND_COLOR.NAVY_3};
-  border-radius: 12px;
   // padding 의 경우 figma상의 최소치 설정
   padding: 86px 78px 40px 78px;
   text-align: center;
   // outLayer보다 높은 z-index 값 부여
   z-index: 5;
+  ${mq('desktop')} {
+    height: 580px;
+    width: 656px;
+    border-radius: 12px;
+  }
 `;
 
 const CloseButton = styled(Close)`
