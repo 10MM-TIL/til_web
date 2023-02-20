5ce0ac068bd48faf2a4f8d23d3032800754c3dcd
haryan248 <haryan96@naver.com>
2023-02-20 14:53:24 +0900

    refactor : test 파일 컴포넌트 분리

--- pages/test.tsx
+++ pages/test.tsx
@@ -8,25 +8,6 @@ import { CertifiedBlog } from '@/components/CertifiedBlog';
 const Test: NextPage = () => {
   // !! Color관련 ThemeProvider 적용할건지 여부
 
-  const [isDeleted1, setDeleted1] = useState(false);
-  const [isDeleted2, setDeleted2] = useState(false);
-  const handleDeleteButton1 = () => {
-    // 특정 조건 이후 없어져야함
-    console.log('blog1 삭제 Post API 전송');
-    setTimeout(() => {
-      console.log('blog1 삭제 완료');
-      setDeleted1(true);
-    }, 2000);
-  };
-  const handleDeleteButton2 = () => {
-    // 특정 조건 이후 없어져야함
-    console.log('blog1 삭제 Post API  전송');
-    setTimeout(() => {
-      console.log('blog2 삭제 완료');
-      setDeleted2(true);
-    }, 2000);
-  };
-
   return (
     <div>
       <div>컴포넌트를 위한 테스트 페이지입니다.</div>
@@ -47,31 +28,54 @@ const Test: NextPage = () => {
       <Typo.Label1 color='violet'>Label2</Typo.Label1>
       <br />
       <Toggle />
+      <CertifiedBlogComponent></CertifiedBlogComponent>
+    </div>
+  );
+};
 
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          gap: 10px;
-          margin-top: 10px;
-        `}
-      >
-        <h1>
-          <strong>인증된 블로그 컴포넌트</strong>
-        </h1>
-        <CertifiedBlog
-          blogName={'github.exaple.com/example1'}
-          blogType={'GitHub'}
-          isDeleted={isDeleted1}
-          onDeleteBlog={handleDeleteButton1}
-        />
-        <CertifiedBlog
-          blogName={'github.exaple.com/example2'}
-          blogType={'GitHub'}
-          isDeleted={isDeleted2}
-          onDeleteBlog={handleDeleteButton2}
-        />
-      </div>
+const CertifiedBlogComponent = () => {
+  const [isDeleted1, setDeleted1] = useState(false);
+  const [isDeleted2, setDeleted2] = useState(false);
+  const handleDeleteButton1 = () => {
+    // 특정 조건 이후 없어져야함
+    console.log('blog1 삭제 Post API 전송');
+    setTimeout(() => {
+      console.log('blog1 삭제 완료');
+      setDeleted1(true);
+    }, 2000);
+  };
+  const handleDeleteButton2 = () => {
+    // 특정 조건 이후 없어져야함
+    console.log('blog1 삭제 Post API  전송');
+    setTimeout(() => {
+      console.log('blog2 삭제 완료');
+      setDeleted2(true);
+    }, 2000);
+  };
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        margin-top: 10px;
+      `}
+    >
+      <h1>
+        <strong>인증된 블로그 컴포넌트</strong>
+      </h1>
+      <CertifiedBlog
+        blogName={'github.exaple.com/example1'}
+        blogType={'GitHub'}
+        isDeleted={isDeleted1}
+        onDeleteBlog={handleDeleteButton1}
+      />
+      <CertifiedBlog
+        blogName={'github.exaple.com/example2'}
+        blogType={'GitHub'}
+        isDeleted={isDeleted2}
+        onDeleteBlog={handleDeleteButton2}
+      />
     </div>
   );
 };
