0fd5d83f6f935653bc264115ef75a113aba71217
sanghyeokahn <jooa7878@naver.com>
2023-03-04 17:54:29 +0900

    feat : typography title 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div>123123123</div>;
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Home;
