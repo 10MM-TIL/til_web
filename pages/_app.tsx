668497c884c5303158e2a58b135e7f411f71bc02
haryan248 <haryan96@naver.com>
2023-03-07 10:04:02 +0900

    style : 오타 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import CommonLayout from '@/components/common/commonLayout';
+import CommonLayout from '@/components/common/CommonLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
