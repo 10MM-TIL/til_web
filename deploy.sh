5683d581a71e0880cbc76422ea2e4dae1ca35afa
haryan248 <haryan96@naver.com>
2023-05-17 18:02:44 +0900

    fix: 오류 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/deploy # 배포된 프로젝트 경로.
+REPOSITORY=/home/ubuntu/deploy
+cd $REPOSITORY
 
-cd $REPOSITORY # 이 경로로 이동해서 밑에 명령어들을 차례로 실행.
-npm run start &&
-pm2 restart til_web
+sudo npm run start &&
+sudo pm2 restart til_web
