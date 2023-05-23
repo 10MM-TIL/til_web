c3cb6c7aef1b6e7091a4b2fab535487f00ace4b6
zerocy18 <zerocy18@gmail.com>
2023-05-24 01:03:56 +0900

    feat : grass click 후 timeline 보여줄 수 있도록 get 파라미터추가

--- hooks/queries/timelineQuery.ts
+++ hooks/queries/timelineQuery.ts
@@ -1,14 +1,22 @@
 import { getUserTimeline } from '@/apis/user';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
-export const useMyAllTimeline = (path: string) => {
-  return useInfiniteQuery(['timelineInfinite', path], ({ pageParam = '' }) => getUserTimeline(path, pageParam), {
-    getNextPageParam: (lastPage) => {
-      if (lastPage) {
-        const { nextPageToken } = lastPage;
-        return nextPageToken === 'null' ? undefined : nextPageToken;
-      }
-      return undefined;
+export const useMyAllTimeline = (path: string, from?: number, to?: number) => {
+  return useInfiniteQuery(
+    ['timelineInfinite', path],
+    ({ pageParam = '' }) => getUserTimeline(path, pageParam, from, to),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage) {
+          const { nextPageToken } = lastPage;
+          return nextPageToken === 'null' ? undefined : nextPageToken;
+        }
+        return undefined;
+      },
     },
-  });
+  );
+};
+
+export const useTimelineByDate = (path: string, from: number, to: number) => {
+  return useQuery(['timelineByDate'], ({ pageParam = '' }) => getUserTimeline(path, pageParam, from, to));
 };
