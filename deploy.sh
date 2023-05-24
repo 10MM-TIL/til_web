9b6c559ffb7d3d3f942e04f6239e468a0e0fe0a8
haryan248 <haryan96@naver.com>
2023-05-24 13:38:56 +0900

    fix: build 명렁어 수정

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/deploy
+DEPLOYMENT_GROUP_NAME="${CODE_DEPLOY_DEPLOYMENT_GROUP_NAME}"
 cd $REPOSITORY
 
-sudo npm install &&
-sudo npm run start:dev &&
-sudo pm2 restart deploy
-
-
+sudo npm install
+if [[ "$DEPLOY_ENVIRONMENT" == "til_fe_prod" ]]; then
+  # production 환경인 경우에 대한 처리
+  echo "Running commands for production environment"
+  sudo npm run pm2:dev
+elif [ $DEPLOYMENT_GROUP_NAME = "til_fe_dev" ]; then
+  echo "Running commands for development environment"
+  sudo npm run pm2:prod
+fi
