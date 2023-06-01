d85d8600798301b4a81632b979c43e165680e416
haryan248 <haryan96@naver.com>
2023-06-01 16:55:56 +0900

    fix: meta 이미지 경로 교체

--- components/Atom/MetaHead/index.tsx
+++ components/Atom/MetaHead/index.tsx
@@ -3,18 +3,18 @@ import React, { ReactElement } from 'react';
 
 export type MetaContents = {
   image?:
-    | '/images/default_meta.png'
-    | '/images/design_meta.png'
-    | '/images/develop_meta.png'
-    | '/images/plan_meta.png'
-    | '/images/startup_meta.png'
-    | '/images/market_meta.png';
+    | '/default_meta.png'
+    | '/design_meta.png'
+    | '/develop_meta.png'
+    | '/plan_meta.png'
+    | '/startup_meta.png'
+    | '/market_meta.png';
   desc?: string;
   title?: string;
   keyword?: string;
 };
 const defaultMetaContents: MetaContents = {
-  image: '/images/default_meta.png',
+  image: '/default_meta.png',
   desc: '꾸준한 회고와 기록을 통해 성장하세요. 매일, 매주, 매달 회고를 인증하고 브릭을 쌓을 수 있어요!',
   title: '브릭로그 - 일잘러의 회고 인증 플랫폼',
   keyword: '브릭로그, 회고, 커리어 성장, TIL, IT, 스타트업, 개발자, 디자이너, 기획자, 마케터',
