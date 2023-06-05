cdcf9d1371ff0783e69b847fe973d24e88b6a280
zerocy18 <zerocy18@gmail.com>
2023-06-05 10:27:08 +0900

    feat : custom 404 page 작업

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,39 @@
 import * as Typo from '@/components/Atom/Typography';
 import { FONT_COLOR } from '@/constants/color';
+import Image from 'next/image';
+import { useResize } from '@/hooks/useResize';
+import { Custom404Wrapper, ImageContainer, TextContainer } from '@/styles/404.module';
+import { Button } from '@/components/Atom/Button';
+import { useCallback } from 'react';
+import { useRouter } from 'next/router';
 
 const Custom404 = () => {
-  return <Typo.H1 color={FONT_COLOR.WHITE}>CUSTOM 404 PAGE</Typo.H1>;
+  const device = useResize();
+  const errorText = `앗, 페이지를 찾을 수 없습니다.\n입력한 주소를 다시 확인해 주세요.`;
+  const router = useRouter();
+  const clickToHome = useCallback(() => {
+    router.push('/');
+  }, [router]);
+
+  return (
+    <>
+      <Custom404Wrapper>
+        <ImageContainer>
+          <Image alt='404' src={require('@/assets/images/404ERROR.png')} />
+        </ImageContainer>
+        <TextContainer>
+          {device === 'desktop' ? (
+            <Typo.Title color={FONT_COLOR.WHITE}>{errorText}</Typo.Title>
+          ) : (
+            <Typo.H1 color={FONT_COLOR.WHITE}>{errorText}</Typo.H1>
+          )}
+        </TextContainer>
+        <Button size='x-lg-m' onClick={clickToHome}>
+          브릭로그 홈으로 이동
+        </Button>
+      </Custom404Wrapper>
+    </>
+  );
 };
 
 export default Custom404;
