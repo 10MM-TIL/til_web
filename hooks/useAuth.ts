a18b7c1e055285693d209f1281eda2a90fadbe64
ahnsanghyeok <jooa7878@naver.com>
2023-06-23 14:43:18 +0900

    feat : accesstoken refresh logic 추가

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,14 +1,28 @@
-import { getCookie } from 'cookies-next';
+import { getCookie, setCookie } from 'cookies-next';
 import { useCallback, useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 
 import { AuthState } from '@/stores/authStateStore';
 import { postAuthRefreshAPI } from '@/apis/auth';
+import { logout } from '@/utils/utils';
+import instance from '@/apis/instance';
 
 const useAuth = () => {
   const setAuthState = useSetRecoilState(AuthState);
 
   const isSignIn = !!getCookie('accToken');
+
+  const handleRenewToken = useCallback(async () => {
+    const res = await postAuthRefreshAPI();
+    if (!res?.data?.accessToken || !res?.data?.refreshToken) {
+      logout(true);
+      return;
+    }
+    const token = res.data.accessToken;
+    setCookie('accToken', token);
+    setCookie('refToken', res.data.refreshToken);
+    instance.defaults.headers['Authorization'] = `Bearer ${token}`;
+  }, []);
 
   useEffect(() => {
     if (isSignIn) {
@@ -16,10 +30,10 @@ const useAuth = () => {
     } else {
       const refToken = getCookie('refToken');
       if (refToken) {
-        postAuthRefreshAPI();
+        handleRenewToken();
       }
     }
-  }, [isSignIn, setAuthState]);
+  }, [handleRenewToken, isSignIn, setAuthState]);
 };
 
 export default useAuth;
