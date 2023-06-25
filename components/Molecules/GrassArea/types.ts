35bd4b5ca16c4185c755c7ba4daa3f92a536e377
zerocy18 <zerocy18@gmail.com>
2023-06-25 17:37:42 +0900

    fear : 잔디 컴포넌트 슬라이딩 기능 제외 후 정상 렌더링 되도록 표시

--- components/Molecules/GrassArea/types.ts
+++ components/Molecules/GrassArea/types.ts
@@ -7,7 +7,7 @@ export type GrassAreaProps = {
 };
 
 // key 이름 수정 필요할듯?
-type GrassStackedKey = '1' | '2' | '3' | '4' | '5';
+type GrassStackedKey = '1' | '2' | '3' | '4';
 export type GrassStackedData = {
   [T in GrassStackedKey]: string[];
 };
