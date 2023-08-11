781b2ae185baeb1c6f13819fd85267005474c9a6
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2023-08-11 20:31:19 +0900

    refactor: 카드 카테고리 api 변경 대응 준비

--- components/Templates/Cardview/CardCategory/index.tsx
+++ components/Templates/Cardview/CardCategory/index.tsx
@@ -6,20 +6,45 @@ import { useQueryClient } from '@tanstack/react-query';
 import { category as CATEGORY, CategoryKeys } from '@/components/Atom/Card/types';
 
 import RadioGroup from '@/components/Molecules/RadioGroup';
-import { useRecoilState } from 'recoil';
+import { SetterOrUpdater, useRecoilState } from 'recoil';
 import { categoryState } from '@/stores/cardviewStateStore';
 import { useCategories } from '@/hooks/queries/categoryQuery';
 import { useRouter } from 'next/router';
 import { findKeyByValue, findSelectedCategory } from '@/utils/cardview';
+import { categories } from '@/types/cardview';
+
+export type RadioComponentProps = { setCategories: SetterOrUpdater<categories[]>; categories: categories[] };
+
+const RadioComponent = ({ setCategories, categories }: RadioComponentProps) => {
+  const router = useRouter();
+  const handleRadioClick = (value: string) => {
+    setCategories(
+      categories.map((category, index) => {
+        if (category.identifier === value) return { ...category, selected: true };
+        else return { ...category, selected: false };
+      }),
+    );
+
+    router.push({
+      query: { category: findKeyByValue(categories.filter((c) => c.identifier === value)[0].name) },
+    });
+  };
+
+  return (
+    <RadioGroup
+      data={categories}
+      selectedId={categories.find((c) => c.selected)?.identifier!}
+      onClick={handleRadioClick}
+    />
+  );
+};
 
 export type CardViewPageProps = { category: CategoryKeys | undefined };
 // 카테고리 버튼
 const CardCategory = ({ category }: CardViewPageProps) => {
-  const queryClient = useQueryClient();
   const [categories, setCategories] = useRecoilState(categoryState);
 
   const { data: categoryData } = useCategories();
-  const router = useRouter();
 
   // category 저장
   useEffect(() => {
@@ -33,29 +58,6 @@ const CardCategory = ({ category }: CardViewPageProps) => {
     }
   }, [categoryData, setCategories, category]);
 
-  const RadioComponent = () => {
-    const handleRadioClick = (value: string) => {
-      setCategories(
-        categories.map((category, index) => {
-          if (category.identifier === value) return { ...category, selected: true };
-          else return { ...category, selected: false };
-        }),
-      );
-
-      router.push({
-        query: { category: findKeyByValue(categories.filter((c) => c.identifier === value)[0].name) },
-      });
-    };
-
-    return (
-      <RadioGroup
-        data={categories}
-        selectedId={categories.find((c) => c.selected)?.identifier!}
-        onClick={handleRadioClick}
-      />
-    );
-  };
-
   return (
     <>
       <Styled.CategoryContainer>
@@ -63,7 +65,7 @@ const CardCategory = ({ category }: CardViewPageProps) => {
           <Typo.H1 color={FONT_COLOR.WHITE}>다른 사람들의 카드</Typo.H1>
         </Styled.CategoryHeader>
         <Styled.CategoryContent>
-          <RadioComponent></RadioComponent>
+          <RadioComponent categories={categories} setCategories={setCategories}></RadioComponent>
         </Styled.CategoryContent>
       </Styled.CategoryContainer>
     </>
