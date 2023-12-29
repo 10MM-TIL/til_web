a2a41921f222f4d1033b68d133496aef3c034203
zerocy18 <zerocy18@gmail.com>
2023-12-30 00:18:56 +0900

    feat: card view 작업

--- components/Templates/Home/HomeCard/index.tsx
+++ components/Templates/Home/HomeCard/index.tsx
@@ -8,25 +8,20 @@ import * as Typo from '@/components/Atom/Typography';
 import { Card } from '@/components/Atom/Card';
 import Footer from '@/components/Atom/Footer';
 
-import { useAllPosts, useRecommandPosts } from '@/hooks/queries/cardviewQuery';
 import { useCategories } from '@/hooks/queries/categoryQuery';
 
 import { elementContainer, elementTitle } from './styles';
 import { MyUserModel } from '@/types/index';
+import { useRecommendRetrospects, useRetrospects } from '@/hooks/queries/retrospectCardviewQuery';
 
 const HomeCard = ({ userData }: { userData?: MyUserModel }) => {
   const router = useRouter();
   const { data: categoryData, isSuccess: isSuccessCategory } = useCategories();
-  const { data: postsData, isSuccess: isSUccessAllpost } = useAllPosts('all');
-  // 'all'로 필요할시 변경해주시면 됩니다.
-  const { data: cardData, isSuccess: isSuccessRecommand } = useRecommandPosts(
+  const { data: retrospectsData, isSuccess: isSuccessRetrospects } = useRetrospects('all');
+  const { data: recommendData, isSuccess: isSuccessRecommand } = useRecommendRetrospects(
     userData ? userData.categoryIdentifier : 'all',
     true,
   );
-
-  const onClickContent = (url: string = '') => {
-    window.open(url);
-  };
 
   const onClickUser = (userpath: string = '') => {
     router.push(`/@${userpath}`);
@@ -41,54 +36,63 @@ const HomeCard = ({ userData }: { userData?: MyUserModel }) => {
             <Typo.Body color={FONT_COLOR.GRAY_2}>더보기</Typo.Body>
           </Link>
         </div>
-        {isSUccessAllpost && postsData.pages[0].posts.length === 0 ? (
+        {isSuccessRetrospects && retrospectsData.pages[0].retrospects.length === 0 ? (
           <Typo.H2 color={FONT_COLOR.GRAY_2}>작성된 회고 글이 없습니다.</Typo.H2>
         ) : (
           isSuccessCategory && (
             <>
               {isSuccessRecommand &&
-                cardData.posts.map((recommandItem, index) => (
-                  <Card
-                    key={recommandItem.createdAt + index + 'mobile'}
-                    size={'sm'}
-                    content={{
-                      category: categoryData.categories.find((i) => i.identifier === recommandItem.categoryIdentifier)
-                        ?.name!,
-                      header: recommandItem.title,
-                      body: recommandItem.summary,
-                      img: recommandItem.profileImgSrc,
-                      name: recommandItem.userName,
-                      date: formatDate(recommandItem.createdAt),
-                    }}
-                    hasBadge={true}
-                    url={recommandItem.url}
-                    userpath={recommandItem.userPath}
-                    onClickContent={() => onClickContent(recommandItem.url)}
-                    onClickUser={() => onClickUser(recommandItem.userPath)}
-                    isPrivate={false}
-                  />
-                ))}
-              {isSUccessAllpost &&
-                postsData?.pages[0].posts?.slice(0, 6)?.map((post, idx) => (
-                  <div key={post?.createdAt + idx + 'desktop'}>
+                recommendData.retrospects.map((recommandItem, index) => {
+                  const bodyText = recommandItem.qna[0].answer;
+
+                  return (
                     <Card
+                      key={recommandItem.createdAt + index + 'mobile'}
                       size={'sm'}
                       content={{
-                        category: categoryData.categories.find((i) => i.identifier === post.categoryIdentifier)?.name!,
-                        header: post.title,
-                        body: post.summary,
-                        img: post.profileImgSrc,
-                        name: post.userName,
-                        date: formatDate(post.createdAt),
+                        category: categoryData.categories.find((i) => i.identifier === recommandItem.categoryIdentifier)
+                          ?.name!,
+                        header: recommandItem.questionTypeName,
+                        body: recommandItem.isSecret ? '비공개 게시글입니다.' : bodyText,
+                        img: '',
+                        name: recommandItem.userName,
+                        date: formatDate(recommandItem.createdAt),
                       }}
-                      url={post.url}
-                      userpath={post.userPath}
-                      onClickContent={() => onClickContent(post.url)}
-                      onClickUser={() => onClickUser(post.userPath)}
+                      hasBadge={true}
+                      url={''}
+                      userpath={recommandItem.userPath}
+                      onClickContent={() => {}}
+                      onClickUser={() => onClickUser(recommandItem.userPath)}
                       isPrivate={false}
                     />
-                  </div>
-                ))}
+                  );
+                })}
+              {isSuccessRetrospects &&
+                retrospectsData?.pages[0].retrospects?.slice(0, 6)?.map((retro, idx) => {
+                  const bodyText = retro.qna[0]?.answer ?? '';
+
+                  return (
+                    <div key={retro?.createdAt + idx + 'desktop'}>
+                      <Card
+                        size={'sm'}
+                        content={{
+                          category: categoryData.categories.find((i) => i.identifier === retro.categoryIdentifier)
+                            ?.name!,
+                          header: retro.questionTypeName,
+                          body: retro.isSecret ? '비공개 게시글입니다.' : bodyText,
+                          img: '',
+                          name: retro.userName,
+                          date: formatDate(retro.createdAt),
+                        }}
+                        url={''}
+                        userpath={retro.userPath}
+                        onClickContent={() => {}}
+                        onClickUser={() => onClickUser(retro.userPath)}
+                        isPrivate={false}
+                      />
+                    </div>
+                  );
+                })}
             </>
           )
         )}
