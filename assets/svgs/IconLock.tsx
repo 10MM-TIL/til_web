deca1425468fd61caa0f0cc68e3f703f1bf493a1
haryan248 <haryan96@naver.com>
2024-01-02 18:18:32 +0900

    fix: svg attribute 수정

--- assets/svgs/IconLock.tsx
+++ assets/svgs/IconLock.tsx
@@ -5,23 +5,23 @@ const IconLock = () => {
         <path
           d='M12.7495 8.24805H5.24951C4.42108 8.24805 3.74951 8.91962 3.74951 9.74805V14.248C3.74951 15.0765 4.42108 15.748 5.24951 15.748H12.7495C13.5779 15.748 14.2495 15.0765 14.2495 14.248V9.74805C14.2495 8.91962 13.5779 8.24805 12.7495 8.24805Z'
           stroke='white'
-          stroke-width='1.35'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.35'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           d='M8.99951 12.75C9.41373 12.75 9.74951 12.4142 9.74951 12C9.74951 11.5858 9.41373 11.25 8.99951 11.25C8.5853 11.25 8.24951 11.5858 8.24951 12C8.24951 12.4142 8.5853 12.75 8.99951 12.75Z'
           stroke='white'
-          stroke-width='1.35'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.35'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path
           d='M5.99951 8.25V5.25C5.99951 4.45435 6.31558 3.69129 6.87819 3.12868C7.4408 2.56607 8.20386 2.25 8.99951 2.25C9.79516 2.25 10.5582 2.56607 11.1208 3.12868C11.6834 3.69129 11.9995 4.45435 11.9995 5.25V8.25'
           stroke='white'
-          stroke-width='1.35'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='1.35'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </g>
     </svg>
