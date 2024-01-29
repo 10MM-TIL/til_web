e64cc260f28b003bc2549b8faf109757c6c65f2f
zerocy18 <zerocy18@gmail.com>
2024-01-29 20:38:59 +0900

    feat: footer 스타일 변경

--- components/Atom/Footer/index.tsx
+++ components/Atom/Footer/index.tsx
@@ -1,19 +1,20 @@
 import Link from 'next/link';
 import { FONT_COLOR } from '@/constants/color';
 import * as Typo from '@/components/Atom/Typography';
-import { footer, divider } from './styles';
+import { footer, divider, wrapper } from './styles';
 
 const Footer = () => {
   return (
-    <footer css={footer}>
-      <Link href='https://www.plip.kr/pcc/c791921f-5dc3-4cb0-baac-55e48ee2e585/privacy-policy' target='_blank'>
-        <Typo.Body color={FONT_COLOR.GRAY_2}>개인정보처리방침</Typo.Body>
-      </Link>
-      <div css={divider} />
-      <Link href='https://10miri.notion.site/a96b7e92cdee4bc2836a0012b8b610b7' target='_blank'>
-        <Typo.Body color={FONT_COLOR.GRAY_2}>서비스 이용 약관</Typo.Body>
-      </Link>
-
+    <footer css={wrapper}>
+      <div css={footer}>
+        <Link href='https://www.plip.kr/pcc/c791921f-5dc3-4cb0-baac-55e48ee2e585/privacy-policy' target='_blank'>
+          <Typo.Body color={FONT_COLOR.GRAY_2}>개인정보처리방침</Typo.Body>
+        </Link>
+        <div css={divider} />
+        <Link href='https://10miri.notion.site/a96b7e92cdee4bc2836a0012b8b610b7' target='_blank'>
+          <Typo.Body color={FONT_COLOR.GRAY_2}>서비스 이용 약관</Typo.Body>
+        </Link>
+      </div>
       <Link href='https://10miri.notion.site/784d36cd04e74f67838cfa5e1b62012b' target='_blank' rel='noreferrer'>
         <Typo.Body color={FONT_COLOR.GRAY_2}>업데이트 노트</Typo.Body>
       </Link>
--- components/Atom/Footer/styles.ts
+++ components/Atom/Footer/styles.ts
@@ -1,16 +1,21 @@
-import { BACKGROUND_COLOR, FONT_COLOR, POINT_COLOR } from '@/constants/color';
+import { BACKGROUND_COLOR } from '@/constants/color';
 import { mq } from '@/styles/mediaQuery';
 import { css } from '@emotion/react';
+
+export const wrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  ${mq('desktop')} {
+    margin-left: 28px;
+  }
+`;
 
 export const footer = css`
   display: flex;
   align-items: center;
   gap: 16px;
-  flex-wrap: wrap;
-
-  ${mq('desktop')} {
-    margin-left: 28px;
-  }
 `;
 export const divider = css`
   width: 1px;
